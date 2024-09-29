c347ea9669b8f98177b6168a76bc08c43c4944f3
DariaUngolo <dariaungolo19@gmail.com>
2024-09-29 20:50:21 +0200

    risolto esercizio

--- ccount.py
+++ ccount.py
@@ -1,17 +1,45 @@
 
 import argparse
 import string
-
+import matplotlib.pyplot as plt
+import time 
 from loguru import logger
 
 
-def count_characters(file_path, plot_histogram):
+#funzione che agisce su due liste 
+def istogramma(x,y):
+    plt.figure(figsize=(12, 6))
+    plt.bar(x, y)
+    plt.xlabel("lettere")
+    plt.ylabel("frequenze")
+    plt.title("Istogramma")
+    plt.show()
+
+
+#funzione che agisce su un dizionario
+def istogramma2(diz):
+    plt.figure(figsize=(12, 6))
+    x=list(diz.keys())
+    y=list(diz.values())
+    plt.bar(x,y)
+    plt.xlabel("lettere")
+    plt.ylabel("frequenze")
+    plt.title("Istogramma")
+    plt.show()
+
+
+ 
+
+
+def count_characters(file_path, plot_histogram, etime):
+    start = time.time()
+   
     counts = {char: 0 for char in string.ascii_lowercase}
     # Rough equivalent
     # counts = {}
     # for char in string.ascii_lowercase:
     #     counts[char] = 0
-    with open(file_path) as input_file:
+    with open(file_path,'r',encoding='ISO-8859-1',errors='ignore') as input_file:
         logger.debug(f'Reading input data from {file_path}...')
         data = input_file.read()
     logger.debug(f'Done, {len(data)} character(s) found.')
@@ -25,15 +53,31 @@ def count_characters(file_path, plot_histogram):
     for key, value in counts.items():
         counts[key] = value / num_characters
     logger.info(f'Character frequences: {counts}')
+    
+    end = time.time()
+    
     if plot_histogram:
-        # Do something...
-        pass
+        #lettere=list(counts.keys())
+        #valori=list(counts.values()) #adesso sono le frequenze, cambiato dentro il ciclo for in cui aggiorno valori del dizionario
+        #istogramma(lettere,valori)
+        istogramma2(counts)
 
+    
+    a= end - start
+    if etime:
+        logger.info(f"Tempo di esecuzione: {a:.6f} secondi")
+
+
+     
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description='Count the characters in a text file')
     parser.add_argument('file')
     parser.add_argument('--histogram', action='store_true',
         help='plot a histogram of the character frequencies')
+    parser.add_argument('--time', action='store_true',
+        help='Tempo di esecuzione per contare i caratteri')
+    
     args = parser.parse_args()
-    count_characters(args.file, args.histogram)
+    count_characters(args.file, args.histogram, args.time)
